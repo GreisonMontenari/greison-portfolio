b06e9e1f8c8f715d7cd9f773f7a19d5b7745033b
Greison Montenari <165381030+GreisonMontenari@users.noreply.github.com>
2025-12-07 07:00:50 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('Versão final será substituída.');
+document.addEventListener('DOMContentLoaded',()=>{
+  document.getElementById('ano').textContent = new Date().getFullYear();
+
+  const form=document.getElementById('contact-form');
+  form.addEventListener('submit',e=>{
+    e.preventDefault();
+    document.getElementById('form-feedback').textContent="Mensagem enviada!";
+    form.reset();
+  });
+});
